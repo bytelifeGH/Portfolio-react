4e78adde4050ac3e049eab190cf4d1ec9200ac52
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2023-09-20 16:33:11 +0530

    Homepage animations updated

--- src/components/home.js
+++ src/components/home.js
@@ -23,7 +23,8 @@ export default function Home(){
         border: "3px solid black",
         transform: "translate3d(0%, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
         transformstyle:"preserve-3d",
-        transition:"all 0.5s"
+        transition:"all 0.5s",
+        zIndex:"1"
       })
       let [style3, setstyle3] = useState({
         width: "40%",
@@ -34,7 +35,8 @@ export default function Home(){
         border: "3px solid black",
         transform: "translate3d(0%, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
         transformstyle:"preserve-3d",
-        transition:"all 0.5s"
+        transition:"all 0.5s",
+        zIndex:"-1"
       })
       let [style4, setstyle4] = useState({
         width: "60%",
@@ -45,7 +47,8 @@ export default function Home(){
         border: "3px solid black",
         transform: "translate3d(0%, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
         transformstyle:"preserve-3d",
-        transition:"all 0.5s"
+        transition:"all 0.5s",
+        zIndex:"1"
       })
       let [style5, setstyle5] = useState({
         width: "40%",
@@ -56,7 +59,8 @@ export default function Home(){
         border: "3px solid black",
         transform: "translate3d(0%, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
         transformstyle:"preserve-3d",
-        transition:"all 0.5s"
+        transition:"all 0.5s",
+        zIndex:"1"
       })
     function hover1(){
         setstyle1(current => {
@@ -243,7 +247,7 @@ export default function Home(){
         setstyle3(current => {
             return (
                 {
-                    zIndex:"1",
+                    zIndex:"-1",
                     width: "50%",
                     height: "50%",
                     position: "absolute",
@@ -257,7 +261,7 @@ export default function Home(){
         setstyle4(current => {
             return (
                 {
-                    zIndex:"1",
+                    zIndex:"2",
                     width: "50%",
                     height: "30%",
                     position: "absolute",
@@ -319,7 +323,7 @@ export default function Home(){
         setstyle3(current => {
             return (
                 {
-                    zIndex:"1",
+                    zIndex:"-1",
                     width: "55%",
                     height: "60%",
                     position: "absolute",
@@ -395,7 +399,7 @@ export default function Home(){
         setstyle3(current => {
             return (
                 {
-                    zIndex:"1",
+                    zIndex:"-1",
                     width: "36%",
                     height: "76%",
                     position: "absolute",
@@ -471,7 +475,7 @@ export default function Home(){
         setstyle3(current => {
             return (
                 {
-                    zIndex:"1",
+                    zIndex:"-1",
                     width: "24%",
                     height: "48%",
                     position: "absolute",
@@ -517,10 +521,14 @@ export default function Home(){
         <>
         <div className="box">
             <a href="/" className="center" onMouseEnter={hover1} onMouseLeave={recover1} style={style1}>Mili</a>
-            <a href="/" className="topleft" onMouseEnter={hover2} onMouseLeave={recover1} style={style2}>Work</a>
-            <a href="/" className="bottomleft" onMouseEnter={hover3} onMouseLeave={recover1} style={style3}>About</a>
-            <a href="/" className="bottomright" onMouseEnter={hover4} onMouseLeave={recover1} style={style4}>Community</a>
-            <a href="/" className="topright" onMouseEnter={hover5} onMouseLeave={recover1} style={style5}>Connect</a>
+            <a href="/" className="topleft" onMouseEnter={hover2} onMouseLeave={recover1} style={style2}>Work
+            <div className="tlin">This is the work page</div></a>
+            <a href="/" className="bottomleft" onMouseEnter={hover3} onMouseLeave={recover1} style={style3}>About
+            <div className="blin">This is the work page</div></a>
+            <a href="/" className="bottomright" onMouseEnter={hover4} onMouseLeave={recover1} style={style4}>Community
+            <div className="brin">This is the work page</div></a>
+            <a href="/" className="topright" onMouseEnter={hover5} onMouseLeave={recover1} style={style5}>Connect
+            <div className="trin">This is the work page</div></a>
         </div>
         </>
     );
